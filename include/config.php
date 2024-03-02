1fd986f7e2197d369091fc4c3eb8c2c9a435b5b9
jarahtech@gmail.com <jarahtech@gmail.com>
2024-03-02 14:36:51 +0600

    first commit-4

--- include/config.php
+++ include/config.php
@@ -2,3 +2,17 @@
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mahabub'] = array ('1'=>'mahabub<|<admin','mahabub>|>eHFyi3lyfnplbmFyeHE=');
 
+$data['RANA-WiFi-1'] = array ('1'=>'RANA-WiFi-1!hen08kkxqc4.sn.mynetname.net','RANA-WiFi-1@|@shafi','RANA-WiFi-1#|#eHE=','RANA-WiFi-1%RANA-WiFi-1','RANA-WiFi-1^login.net','RANA-WiFi-1&QR','RANA-WiFi-1*10','RANA-WiFi-1(1','RANA-WiFi-1)','RANA-WiFi-1=10','RANA-WiFi-1@!@enable');
+$data['Zaheer-NET-1'] = array ('1'=>'Zaheer-NET-1!hd608da97en.sn.mynetname.net','Zaheer-NET-1@|@shafi','Zaheer-NET-1#|#eHE=','Zaheer-NET-1%Zaheer-NET-1','Zaheer-NET-1^login.net','Zaheer-NET-1&QR','Zaheer-NET-1*10','Zaheer-NET-1(1','Zaheer-NET-1)','Zaheer-NET-1=10','Zaheer-NET-1@!@enable');
+
+$data['Zaheer-WiFi-2'] = array ('1'=>'Zaheer-WiFi-2!hek08j4r794.sn.mynetname.net','Zaheer-WiFi-2@|@shafi','Zaheer-WiFi-2#|#eHE=','Zaheer-WiFi-2%Zaheer-WiFi-2','Zaheer-WiFi-2^login.net','Zaheer-WiFi-2&QR','Zaheer-WiFi-2*10','Zaheer-WiFi-2(1','Zaheer-WiFi-2)','Zaheer-WiFi-2=10','Zaheer-WiFi-2@!@enable');
+$data['Rana-WiFi-2'] = array ('1'=>'Rana-WiFi-2!hf6094yzgwr.sn.mynetname.net','Rana-WiFi-2@|@shafi','Rana-WiFi-2#|#eHE=','Rana-WiFi-2%Rana-WiFi-2','Rana-WiFi-2^login.net','Rana-WiFi-2&QR','Rana-WiFi-2*10','Rana-WiFi-2(1','Rana-WiFi-2)','Rana-WiFi-2=10','Rana-WiFi-2@!@enable');
+$data['SHOHAG-5G'] = array ('1'=>'SHOHAG-5G!hf6097a49n4.sn.mynetname.net','SHOHAG-5G@|@shafi','SHOHAG-5G#|#eHE=','SHOHAG-5G%SHOHAG-5G','SHOHAG-5G^login.net','SHOHAG-5G&QR','SHOHAG-5G*10','SHOHAG-5G(1','SHOHAG-5G)','SHOHAG-5G=10','SHOHAG-5G@!@enable');
+$data['PC-MikroTik'] = array ('1'=>'PC-MikroTik!78.101.65.40','PC-MikroTik@|@shafi','PC-MikroTik#|#eHE=','PC-MikroTik%Zaheer-WiFi-PC','PC-MikroTik^login.net','PC-MikroTik&QR','PC-MikroTik*10','PC-MikroTik(1','PC-MikroTik)','PC-MikroTik=10','PC-MikroTik@!@enable');
+$data['DOHA-NET'] = array ('1'=>'DOHA-NET!hf609cpxx1a.sn.mynetname.net','DOHA-NET@|@shafi','DOHA-NET#|#eHE=','DOHA-NET%DOHA-NET','DOHA-NET^login.net','DOHA-NET&QR','DOHA-NET*10','DOHA-NET(1','DOHA-NET)','DOHA-NET=10','DOHA-NET@!@enable');
+$data['RANA-WiFi-3'] = array ('1'=>'RANA-WiFi-3!hdf08j98m5s.sn.mynetname.net','RANA-WiFi-3@|@shafi','RANA-WiFi-3#|#eHE=','RANA-WiFi-3%RANA-WiFi-3','RANA-WiFi-3^login.net','RANA-WiFi-3&QR','RANA-WiFi-3*10','RANA-WiFi-3(1','RANA-WiFi-3)','RANA-WiFi-3=10','RANA-WiFi-3@!@enable');
+$data['RANA-WiFi-4'] = array ('1'=>'RANA-WiFi-4!hd7083m3fc4.sn.mynetname.net','RANA-WiFi-4@|@shafi','RANA-WiFi-4#|#eHE=','RANA-WiFi-4%RANA-WiFi-4','RANA-WiFi-4^login.net','RANA-WiFi-4&QR','RANA-WiFi-4*10','RANA-WiFi-4(1','RANA-WiFi-4)','RANA-WiFi-4=10','RANA-WiFi-4@!@enable');
+
+$data['WiFi'] = array ('1'=>'WiFi!92.99.83.120','WiFi@|@admin','WiFi#|#mZOVnHFYaWNl','WiFi%WiFi','WiFi^login.net','WiFi&Rp','WiFi*10','WiFi(1','WiFi)','WiFi=10','WiFi@!@enable');
+$data['SPEED'] = array ('1'=>'SPEED!103.48.160.62:5017','SPEED@|@admin','SPEED#|#maubsnFjamRm','SPEED%speed','SPEED^login.net','SPEED&aed','SPEED*10','SPEED(1','SPEED)','SPEED=10','SPEED@!@enable');
+$data['ZAHEER-WiFi'] = array ('1'=>'ZAHEER-WiFi!heb08yr2t31.sn.mynetname.net','ZAHEER-WiFi@|@shafi','ZAHEER-WiFi#|#eHE=','ZAHEER-WiFi%SANAYA-44','ZAHEER-WiFi^login.net','ZAHEER-WiFi&QR','ZAHEER-WiFi*10','ZAHEER-WiFi(1','ZAHEER-WiFi)','ZAHEER-WiFi=10','ZAHEER-WiFi@!@enable');
