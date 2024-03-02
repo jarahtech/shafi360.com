3f86f570a8a0107d96d56985d1433558b9ca97af
jarahtech@gmail.com <jarahtech@gmail.com>
2024-03-02 14:27:48 +0600

    first commit-3

--- include/config.php
+++ include/config.php
@@ -1,4 +1,4 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mahabub'] = array ('1'=>'mahabub<|<admin','mahabub>|>aWNlbA==');
+$data['mahabub'] = array ('1'=>'mahabub<|<admin','mahabub>|>eHFyi3lyfnplbmFyeHE=');
 
